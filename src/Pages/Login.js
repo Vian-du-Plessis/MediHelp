9e61b8599c0b1f78589017304f36c06d8e8c6c91
Vian-du-Plessis <21100483@virtualwindow.co.za>
2022-05-15 15:48:03 +0200

    Added Images

---
+++ src/Pages/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Login.module.scss';
+
+const Login = () => {
+    return (
+        <div className=''>
+            
+        </div>
+    );
+};
+
+export default Login;
